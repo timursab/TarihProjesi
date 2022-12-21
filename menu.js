e52a2f6bed26828a1e17ef9cde157cf5f6eeeb38
Timur <84009993+timursab@users.noreply.github.com>
2022-12-21 23:09:47 +0300

    update

--- menu.js
+++ menu.js
@@ -4,16 +4,19 @@ document.body.append(input)
 
 input.type = 'file';
 input.accept = '.txt';
-
+var lines;
 input.onchange = e =>{
     var file = e.target.files[0];
     var reader = new FileReader();
     reader.readAsText(file,'UTF-8');
     reader.onload = readerevent =>{
         var content = readerevent.target.result;
-        var lines = content.split('\n');
+        lines = content.split('-');
         for (var line = 0; line < lines.length; line++) {
             console.log(lines[line]);
           }
+        console.log(lines)
+        localStorage.setItem("lines",lines);
     }
+
 }
